23887c8a2387542e0452ccb6fd6f6c17c697ad46
jxh602 <jxh602@Atlas4-optiplex.(none)>
2015-02-16 20:03:57 -0500

    Some minor fixes to the turning time

--- catkin/src/cwru_376_student/example_robot_commander/src/robot_controller_jinx.cpp
+++ catkin/src/cwru_376_student/example_robot_commander/src/robot_controller_jinx.cpp
@@ -47,7 +47,7 @@ for (int i=0;i<niters;i++) {
 }
 twist_cmd.linear.x = 0.0;
 twist_cmd.angular.z = -0.314;
-niters=500; // 5 sec
+niters=480; // 5 sec
 ROS_INFO("Time to rotate negative");
 for (int i=0;i<niters;i++) {
     cmd_publisher.publish(twist_cmd); // really, should only need to publish this once, but no hard done
@@ -70,7 +70,7 @@ for (int i = 0; i < niters; i++) {
 
 twist_cmd.linear.x = 0.0;
 twist_cmd.angular.z = -0.314;
-niters=500; // 5 sec
+niters=475; // 5 sec
 ROS_INFO("Time to rotate negative (again)!");
 for (int i=0;i<niters;i++) {
     cmd_publisher.publish(twist_cmd); // really, should only need to publish this once, but no hard done
