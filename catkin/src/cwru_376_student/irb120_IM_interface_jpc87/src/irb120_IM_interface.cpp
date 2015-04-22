ed6e4bfe5d3083943da737cf242f05bd8be571f4
remix224 <remix224@gmail.com>
2015-04-22 19:28:44 -0400

    Added arm changes

--- catkin/src/cwru_376_student/irb120_IM_interface_jpc87/src/irb120_IM_interface.cpp
+++ catkin/src/cwru_376_student/irb120_IM_interface_jpc87/src/irb120_IM_interface.cpp
@@ -35,10 +35,12 @@ Eigen::Quaterniond g_quat,g_quat2;
 Eigen::Matrix3d g_R,g_R2;
 Eigen::Affine3d g_A_flange_desired,g_A_flange_desired2;
 tf::StampedTransform bltolink1_;
-tf::Point link1_point;
-tf::TransformListener* tfListener_;
+tf::TransformListener* g_tfListener;
+int test = 1;
 
-bool g_trigger=false;
+geometry_msgs::PoseStamped g_marker_pose_wrt_arm_base;
+
+int g_trigger=0;
 using namespace std;
 
 /*void markerListenerCB(
@@ -55,59 +57,89 @@ using namespace std;
     g_quat.z() = feedback->pose.orientation.z;
     g_quat.w() = feedback->pose.orientation.w;   
     g_R = g_quat.matrix();
-} */
+}*/
 
 void jointStateCB(
 const sensor_msgs::JointStatePtr &js_msg) { //THIS IS NOT GETTING CALLED!!!! I DONT KNOW WHY... FIX!!!
     
     for (int i=0;i<6;i++) {
-        g_q_state[i] = js_msg->position[i];
-        //ROS_INFO("q_p_state is %f for %d",g_q_state[i],i);
+        g_q_state[i] = js_msg->position[i+4]; //Based on the data, the first joint starts at index 4
+        //ROS_INFO("q_p_state is %f for %d",g_q_state[i + 4],i);
     }
     //cout<<"g_q_state: "<<g_q_state.transpose()<<endl;
     
 }
 
 //obtain point to reach from above
-void canListenerCB(const geometry_msgs::Pose pose){
+void canListenerCB(const geometry_msgs::PoseStamped &g_marker_pose_in){
     ROS_INFO("canListenerCB callback activated");
 
-    //g_A_flange_desired.translation() = g_p;
-    //g_A_flange_desired.linear() = g_R;
-    //link1_point = bltolink1_ * pose;
+    g_tfListener->transformPose("link1", g_marker_pose_in, g_marker_pose_wrt_arm_base);
 
-    g_p[0] = pose.position.x;
-    g_p[1] = pose.position.y;
-    g_p[2] = pose.position.z + hand_offset + elevation_approach;
-    g_quat.x() = 0;//pose.orientation.x;
+    g_p[0] = g_marker_pose_wrt_arm_base.pose.position.x;
+    g_p[1] = g_marker_pose_wrt_arm_base.pose.position.y;
+    g_p[2] = g_marker_pose_wrt_arm_base.pose.position.z + hand_offset + elevation_approach;
+    g_quat.x() = 1;//pose.orientation.x;
     g_quat.y() = 0;//pose.orientation.y;
-    g_quat.z() = 1;//pose.orientation.z;
+    g_quat.z() = 0;//pose.orientation.z;
     g_quat.w() = 0;//pose.orientation.w;  
     g_R = g_quat.matrix();
 
     g_A_flange_desired.translation() = g_p;
     g_A_flange_desired.linear() = g_R;
 
-    g_p2[0] = pose.position.x;
-    g_p2[1] = pose.position.y;
-    g_p2[2] = pose.position.z + (hand_offset);
-    g_quat2.x() = 0;//pose.orientation.x;
+    g_p2[0] = g_marker_pose_wrt_arm_base.pose.position.x;
+    g_p2[1] = g_marker_pose_wrt_arm_base.pose.position.y;
+    g_p2[2] = g_marker_pose_wrt_arm_base.pose.position.z + (hand_offset);
+    g_quat2.x() = 1;//pose.orientation.x;
     g_quat2.y() = 0;//pose.orientation.y;
-    g_quat2.z() = 1;//pose.orientation.z;
+    g_quat2.z() = 0;//pose.orientation.z;
     g_quat2.w() = 0;//pose.orientation.w;  
     g_R2 = g_quat.matrix();
 
     g_A_flange_desired2.translation() = g_p2;
     g_A_flange_desired2.linear() = g_R2;
 
-    g_trigger=1; //flag 
+    g_trigger=1.0; //flag 
 }
 
 bool triggerService(cwru_srv::simple_bool_service_messageRequest& request, cwru_srv::simple_bool_service_messageResponse& response){
     response.resp = true;
     //g_A_flange_desired.translation() = g_p;
     //g_A_flange_desired.linear() = g_R;
-    g_trigger=2; //flag
+    /////TEST
+    if(test == 0){
+        test = 1;
+        g_p[0] = .25 - hand_offset - elevation_approach;
+        g_p[1] = 0;
+        g_p[2] = .8;
+        g_quat.x() = 0;//pose.orientation.x;
+        g_quat.y() = 0;//pose.orientation.y;
+        g_quat.z() = 0;//pose.orientation.z;
+        g_quat.w() = 1;//pose.orientation.w;  
+        g_R = g_quat.matrix();
+
+        g_A_flange_desired.translation() = g_p;
+        g_A_flange_desired.linear() = g_R;
+
+        g_p2[0] = .25 - hand_offset;
+        g_p2[1] = 0;
+        g_p2[2] = .8;
+        g_R2 = g_R;
+
+        g_A_flange_desired2.translation() = g_p2;
+        g_A_flange_desired2.linear() = g_R2;
+
+        g_trigger=1.0; //flag */
+    }
+    else {
+        test = 0;
+        g_trigger=2.0;
+    }
+    
+
+    /*
+    g_trigger=2.0; //flag*/
     return true;
 }
 
@@ -131,6 +163,7 @@ double getTimeTraversalFromJoints(Vectorq6x1 initial_state, Vectorq6x1 end){
         }
         
         duration +=  weight * std::abs(initial_state[joints] - end[joints]);
+        ROS_INFO("joint %d, init %f, end %f",joints,initial_state[joints], end[joints]);
         
     }
     ROS_INFO("time is %f",duration);
@@ -151,12 +184,23 @@ void stuff_trajectory( std::vector<Vectorq6x1> qvec_array, int number_of_vecs, t
 
     new_trajectory.header.stamp = ros::Time::now();
 
+    Vectorq6x1 initial_state;
+
+    for (int ijnt=0;ijnt<6;ijnt++) {
+            initial_state[ijnt] = g_q_state[ijnt];//Update the new current position after move is made
+        } 
+
     Vectorq6x1 qvec;
     double total_duration = 0;
-    Vectorq6x1 initial_state = g_q_state; 
+    //Vectorq6x1 initial_state = g_q_state; 
     for(int vec = 0;vec < number_of_vecs;vec++){ //Lets traverse over the vectors that came in
         qvec = qvec_array[vec];//Get the nth element
         double time_to_traverse = getTimeTraversalFromJoints(initial_state,qvec); //Get how much time this should take
+        if(time_to_traverse > 50){
+            ROS_INFO("Took too long, aborting");
+            new_trajectory.points.clear();
+            return;
+        }
         double dt = .2; //Lets split things up
         double points = time_to_traverse/dt; //take our calculated time it takes and subdivide into points
 
@@ -230,15 +274,16 @@ int main(int argc, char** argv) {
     ros::NodeHandle nh;
     ros::Publisher pub = nh.advertise<trajectory_msgs::JointTrajectory>("joint_path_command", 1); 
 
-    tfListener_ = new tf::TransformListener;  //create a transform listener
     bool tferr=true;
-    while (tferr && ros::ok()){
+    ROS_INFO("waiting for tf between base_link and link1 of arm...");
+    g_tfListener = new tf::TransformListener;  //create a transform listener
+    while (tferr) {
         tferr=false;
         try {
                 //try to lookup transform from target frame "odom" to source frame "map"
             //The direction of the transform returned will be from the target_frame to the source_frame. 
              //Which if applied to data, will transform data in the source_frame into the target_frame. See tf/CoordinateFrameConventions#Transform_Direction
-                tfListener_->lookupTransform("link1", "base_link", ros::Time(0), bltolink1_); //after this we can now transform baselink codes
+                g_tfListener->lookupTransform("base_link", "link1", ros::Time(0), bltolink1_);
             } catch(tf::TransformException &exception) {
                 ROS_ERROR("%s", exception.what());
                 tferr=true;
@@ -246,6 +291,7 @@ int main(int argc, char** argv) {
                 ros::spinOnce();                
             }   
     }
+    ROS_INFO("tf is good");
 
     ROS_INFO("setting up subscribers ");
     ros::Subscriber sub_js = nh.subscribe("/joint_states",1,jointStateCB);
@@ -255,14 +301,14 @@ int main(int argc, char** argv) {
     
     Eigen::Vector3d p;
     Eigen::Vector3d n_des,t_des,b_des;
-    std::vector<Vectorq6x1> q6dof_solns;
+    std::vector<Vectorq6x1> q6dof_solns, q6dof_solns2;
     std::vector<Vectorq6x1> q6dof_desired_poses(2); //Array of poses to feed in
     Vectorq6x1 qvec;
     Vectorq6x1 tvec;
-    Vectorq6x1 homevec;
+    Vectorq6x1 homevec,homevecup;
     ros::Rate sleep_timer(frequency); //10Hz update rate    
     Irb120_fwd_solver irb120_fwd_solver; //instantiate forward and IK solvers
-    Irb120_IK_solver ik_solver;
+    Irb120_IK_solver ik_solver, ik_solver2;
     Eigen::Vector3d n_urdf_wrt_DH,t_urdf_wrt_DH,b_urdf_wrt_DH;
     // in home pose, R_urdf = I
     //DH-defined tool-flange axes point as:
@@ -282,10 +328,15 @@ int main(int argc, char** argv) {
     R_urdf_wrt_DH.col(1) = t_urdf_wrt_DH;
     R_urdf_wrt_DH.col(2) = b_urdf_wrt_DH;    
 
+    R_urdf_wrt_DH2.col(0) = n_urdf_wrt_DH;
+    R_urdf_wrt_DH2.col(1) = t_urdf_wrt_DH;
+    R_urdf_wrt_DH2.col(2) = b_urdf_wrt_DH;
+
     trajectory_msgs::JointTrajectory new_trajectory; // an empty trajectory
 
+    homevecup<<0,-M_PI/3,-M_PI/3,0,M_PI/3,0;
+    homevec<<-1.6,-1.6,-1,0,1,0; //home state... deriably with 0, -pi/2 , pi/2 to start. This is used so that it can always get to the table from above
     
-    homevec<<0,-M_PI/3,-M_PI/3,0,M_PI/3,0; //home state... deriably with 0, -pi/2 , pi/2 to start. This is used so that it can always get to the table from above
     Eigen::Affine3d A_flange_des_DH,A_flange_des_DH2;
 
     
@@ -295,40 +346,45 @@ int main(int argc, char** argv) {
     while(ros::ok()) {
             ros::spinOnce();
 
-            if (g_trigger==1) {
-                // ooh!  excitement time!  got a new tool pose goal!
-                
-                //is this point reachable?
+            if (g_trigger!=0) {
+                if(g_trigger==1){
+                    // ooh!  excitement time!  got a new tool pose goal!
+                    
+                    //is this point reachable?
 
-                A_flange_des_DH = g_A_flange_desired;
-                A_flange_des_DH.linear() = g_A_flange_desired.linear()*R_urdf_wrt_DH.transpose();
-                nsolns = ik_solver.ik_solve(A_flange_des_DH);
-                A_flange_des_DH2 = g_A_flange_desired2;
-                A_flange_des_DH2.linear() = g_A_flange_desired2.linear()*R_urdf_wrt_DH2.transpose();
-                nsolns2 = ik_solver.ik_solve(A_flange_des_DH2);
-                ROS_INFO("there are %d solutions",nsolns);
+                    A_flange_des_DH = g_A_flange_desired;
+                    A_flange_des_DH.linear() = g_A_flange_desired.linear()*R_urdf_wrt_DH.transpose();
+                    nsolns = ik_solver.ik_solve(A_flange_des_DH);
 
-                if (nsolns>0) {      
-                    ik_solver.get_solns(q6dof_solns);
-                    qvec = q6dof_solns[findOptimalSolution(q6dof_solns, nsolns)]; // Choose the first solution wisely
-                    tvec = q6dof_solns[findOptimalSolution(q6dof_solns, nsolns2)];
+                    A_flange_des_DH2 = g_A_flange_desired2;
+                    A_flange_des_DH2.linear() = g_A_flange_desired2.linear()*R_urdf_wrt_DH2.transpose();
+                    nsolns2 = ik_solver2.ik_solve(A_flange_des_DH2);
 
-                    q6dof_desired_poses[0] = qvec; //first go to a point above the object
-                    q6dof_desired_poses[1] = tvec; //approach the object and stop
-                                   
-                    stuff_trajectory(q6dof_desired_poses,2,new_trajectory, 4); //2 poses feed in with a 2 sec delay in between each one
- 
-                        pub.publish(new_trajectory);
+                    ROS_INFO("there are %d and %d solutions",nsolns, nsolns2);
+
+                    if (nsolns>0 && nsolns2>0) {      
+                        ik_solver.get_solns(q6dof_solns);
+                        qvec = q6dof_solns[findOptimalSolution(q6dof_solns, nsolns)]; // Choose the first solution wisely
+
+                        ik_solver2.get_solns(q6dof_solns2);
+                        tvec = q6dof_solns2[findOptimalSolution(q6dof_solns2, nsolns2)];
+
+                        q6dof_desired_poses[0] = qvec; //first go to a point above the object
+                        q6dof_desired_poses[1] = tvec; //approach the object and stop
+                                       
+                        stuff_trajectory(q6dof_desired_poses,2,new_trajectory, 1); //2 poses feed in with a 4 sec delay in between each one
+                    }
                 }
-                g_trigger=0; // reset the trigger
-            }
-            else if(g_trigger == 2){
-                q6dof_desired_poses[0] = homevec;
-                stuff_trajectory(q6dof_desired_poses,2,new_trajectory, 4); //2 poses feed in with a 2 sec delay in between each one
+                else if(g_trigger == 2){
+                    q6dof_desired_poses[0] = homevecup;
+                    q6dof_desired_poses[1] = homevec;
+                    stuff_trajectory(q6dof_desired_poses,2,new_trajectory, 1); //2 poses feed in with a 4 sec delay in between each one
+                }
                 pub.publish(new_trajectory);
+                g_trigger=0; // reset the trigger
+                  
             }
-            sleep_timer.sleep();    
-            
+            sleep_timer.sleep(); 
     }
     
     return 0;
